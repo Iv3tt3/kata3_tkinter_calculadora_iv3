03ab61da1e7fca3b151458135421200fc482a876
Ivette Farre Prat <ivettefarreprat@Ivettes-MacBook-Pro.local>
2023-06-12 19:54:53 +0100

    calculadora version aceptable, se puede mejorar

--- calculator/__init__.py
+++ calculator/__init__.py
@@ -1,6 +1,6 @@
 import tkinter as tk
 from calculator.controls import Display, KeyBoard
-from class_romannumbers import RomanNumber
+from class_romannumbers import RomanNumber, RomanNumberError
 
 WIDTH = 272
 HEIGHT = 300
@@ -19,43 +19,56 @@ class Calculator(tk.Tk):
         self.title("Calculadora")
         self.geometry(f"{WIDTH}x{HEIGHT}")
 
-        self.op1 = None
-        self.op2 = None
-        self.operation = None
-
-        self.value = ""
+        self.reset()
 
         self.display = Display(self)
         self.display.pack()
 
-        self.display.typing("Probando")
+        self.display.typing("Hola")
 
         KeyBoard(self, self.click).pack()
 
-        
+    def reset(self):
+        self.value = ''
+        self.op1 = self.op2 = self.operation = None
+        self.equal = False
     
     def click(self, tecla):
-        if tecla == 'AC':
-            self.value = ''
-            self.op1 = self.op2 = self.operation = None
-        elif tecla in operaciones:
-            if self.operation:
-            #Es lo mismo que poner if self.operation is not None
-                self.op2 = RomanNumber(self.value)
-                resultado = operaciones[self.operation](self.op1, self.op2)
-                self.op1 = resultado
-                self.op2 = None
+        try:
+            if tecla == 'AC':
+                self.reset()
+            elif tecla in operaciones:
+                if self.operation:
+                #Es lo mismo que poner if self.operation is not None
+                    if self.equal:
+                        resultado = RomanNumber(self.value)
+                    else:
+                        self.op2 = RomanNumber(self.value)
+                        resultado = operaciones[self.operation](self.op1, self.op2)
+                    self.op1 = resultado
+                    self.op2 = None
+                    self.value = resultado.simbolo
+                else:
+                    self.op1 = RomanNumber(self.value)
+                self.operation = tecla
+            elif tecla == '=':
+                if self.equal:
+                    resultado = operaciones[self.operation](RomanNumber(self.value), self.op2)
+                else:
+                    self.op2 = RomanNumber(self.value)
+                    resultado = operaciones[self.operation](self.op1, self.op2)
                 self.value = resultado.simbolo
             else:
-                self.op1 = RomanNumber(self.value)
-            self.operation = tecla
-        elif tecla == '=':
-            self.op2 = RomanNumber(self.value)
-            resultado = operaciones[self.operation](self.op1, self.op2)
-            self.value = resultado.simbolo
-        else:
-            if self.operation is not None and self.op2 is None:
-                self.value = ''
-                self.op2 = '' #Le damos un valor '' para que sea distinto de None
-            self.value += tecla
-        self.display.typing(self.value) 
+                if self.equal:
+                    self.reset()
+                if self.operation is not None and self.op2 is None:
+                    self.value = ''
+                    self.op2 = '' #Le damos un valor '' para que sea distinto de None
+                self.value += tecla
+            self.display.typing(self.value) 
+            self.equal = tecla == '=' #Esto es una operacion logica, si la tecla es igual a = va a ser True
+        except RomanNumberError:
+            self.value = 'ERROR'
+            self.display.typing(self.value) 
+            self.reset()
+
